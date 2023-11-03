297639cc6b20f3d996a5887c99cae77d2fcd8fca
Ricardo Arcones <rarcones@gm.com>
2023-11-03 20:39:20 +0100

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,18 @@ import './style/style.css'
 
 export default function Home() {
   return (
-    <section>
-      <div>
-        <h1>Blog personal de viajes</h1>
-        <h2>Entradas</h2>
-        <p>Mi página personal de viajes por lugares fantásticos</p>
-      </div>
-    </section>)
+    <body>
+      <h1>Blog personal de viajes</h1>
+      <section>
+        <div>
+          <h2>Presentacion:</h2>
+          <p>Bienvenido a mi página personal sobre viajes en el mundo fantástico.</p>
+          <p>Aquí encontrarás ciudades y lugares emblematicos para visitar, si decides acercarte a ellos.</p>
+          <p>Aviso: no hay billete de avión ni medio de transporte que te permita llegar a estos lugares, tendrás que usar la imaginación, los comics y las peliculas si quieres pasar por alli... ¡disftuta!</p>
+        </div>
+      </section>
+      <footer>
+        <small>Ricardo Arcones Fernandez 2023</small>
+      </footer>
+    </body>)
 }
