f980c5e66fd045bd80f27a40e2d2ce30e73899e3
Ricardo Arcones <rarcones@gm.com>
2023-11-03 22:14:39 +0100

    push

--- src/pages/metropolis.js
+++ src/pages/metropolis.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 export default function metropolis() {
   return (
-    <div>
-      <h1>METROPOLIS</h1>
-      <p>La ciudad de Superman</p>
-    </div>
+    <body>
+        <div>
+        <h1>METROPOLIS</h1>
+        <p>La ciudad de Superman</p>
+        <section>
+            <div>
+            <img src="wakanda.jpg"  alt="Una foto de la ciudad de Metropolis"/>
+            </div>
+        </section>
+        </div>
+    </body>
   )
 }
